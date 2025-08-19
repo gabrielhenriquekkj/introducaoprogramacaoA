1abe818401e0975058e02633e7a6b8e014857b34
gabrielhenriquekkj <lealarrudagabrielhenrique@gmail.com>
2025-08-19 10:08:01 -0300

    Add files via upload

---
+++ gabriel henrique info a questão nota que falta.py
@@ -0,0 +1,5 @@
+n1 = int(input("digite a nota do primeiro bimestre:"))
+n2 = int(input("digite a nota do segundo bimestre:"))
+n = n2*2+n1*2
+conta = 60 - n
+print("a nota necessária para os dois ultimos bimestres é:",conta/6)
