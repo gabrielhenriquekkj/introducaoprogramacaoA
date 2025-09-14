863ebfd2c25c7589086abc5d5ef3e6c7d2d5f87c
gabrielhenriquekkj <lealarrudagabrielhenrique@gmail.com>
2025-09-14 11:30:32 -0300

    Add files via upload

---
+++ for i in range exercicio 4.py
@@ -0,0 +1,6 @@
+z = 0
+x = int(input("digite o número de jogadores do time:"))
+for i in range (x):
+    y = float(input("digite a altura dos jogadores:"))
+    z+=y
+print("a altura média dos jogadores é:",z/x)
