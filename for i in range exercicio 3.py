1042bbec7dd2f6a3682a0be16d9d4aad5ded420e
gabrielhenriquekkj <lealarrudagabrielhenrique@gmail.com>
2025-09-14 11:25:47 -0300

    Add files via upload

---
+++ for i in range exercicio 3.py
@@ -0,0 +1,5 @@
+inte = 0
+n = int(input("digite um número para que se verifique os números entre 1 e ele:"))
+for i in range(1,n+1):
+    inte += i 
+print("a soma dos números inteiros nesse intervalo é:",inte)
